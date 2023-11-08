d7d8a186973e6807435f79aa96ba3e820327a359
Eric Tran <134130254+Nisloen@users.noreply.github.com>
2023-11-08 18:38:00 +1100

    Add files via upload

---
+++ API.js
@@ -0,0 +1,244 @@
+function sendTextToAPI() {
+    var userInput1 = document.getElementById("user_input1").value;
+    var userInput2 = document.getElementById("user_input2").value;
+    var apiUrl = 'http://127.0.0.1:5000/';  
+
+    
+    var request = new Request(apiUrl, {
+        method: 'POST', 
+        body: JSON.stringify({text1: userInput1, text2: userInput2 }), 
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+   
+    fetch(request)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            
+            createHighchartsBarChart(data.dataframe, 'barChartContainer');
+            createHighchartsGauge(data.dataframe, 'gaugeChartContainer');
+            updateReviews(data.dataframe);
+            console.log(data.dataframe);
+
+            document.getElementById("urlDisplay").style.display = "none";
+        })
+        .catch(function(error) {
+            console.error('Error:', error);
+    });
+       
+};
+
+function createHighchartsBarChart(data, containerId) {
+    const sentimentCounts = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    data.forEach(row => {
+        sentimentCounts[row.Output]++;
+    });
+
+    const labels = Object.keys(sentimentCounts);
+    const values = Object.values(sentimentCounts);
+
+    Highcharts.chart(containerId, {
+        chart: {
+            type: 'bar',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            width: 600
+        },
+        title: {
+            text: 'Total star count based on reviews',
+            style: {
+                color: 'white',
+            },
+        },
+        xAxis: {
+            categories: labels.map(label => `${label}`),
+            title: {
+                text: 'Star rating',
+                style: {
+                    color: 'white',
+                },
+            },
+            labels: {
+                style: {
+                    color: 'white', 
+                },
+            },
+        },
+        yAxis: {
+            categories: labels.map(label => `${label}`),
+            title: {
+                text: 'Star rating2',
+                style: {
+                    color: 'white',
+                },
+            },
+            labels: {
+                style: {
+                    color: 'white', 
+                },
+            },
+        },
+        series: [{
+            name: 'Count',
+            data: values,
+        }]
+    });
+};
+
+function createHighchartsGauge(data, containerId) {
+    const totalSentiment = data.reduce((sum, row) => sum + row.Output, 0);
+    const averageSentiment = totalSentiment / data.length;
+    const minRange = 1;
+    const maxRange = 5;
+
+    Highcharts.chart(containerId, {
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: 'rgba(0, 0, 0, 0.1)',
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false,
+            width:600
+        },
+        title: {
+            text: 'Average Star Rating'
+        },
+        pane: {
+            startAngle: -90,
+            endAngle: 90,
+            background: [{
+                outerRadius: '100%',
+                innerRadius: '0%',
+                shape: 'arc',
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#DDD']
+                    ]
+                }
+            }]
+        },
+        yAxis: {
+            min: minRange,
+            max: maxRange,
+            title: {
+                text: 'Rating'
+            },
+        },
+        plotOptions: {
+            gauge: {
+                dataLabels: {
+                    enabled: false
+                },
+                dial: {
+                    radius: '100%',
+                    backgroundColor: 'black',
+                    borderColor: 'silver',
+                    borderWidth: 1,
+                    baseWidth: 10,
+                    topWidth: 1,
+                    rearLength: '0'
+                }
+            }
+        },
+        series: [{
+            name: 'Average Rating',
+            data: [averageSentiment],
+            tooltip: {
+                valueSuffix: ' stars'
+            }
+        }]
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const readMoreLinks = document.querySelectorAll('.read-more');
+    const readLessLinks = document.querySelectorAll('.read-less');
+    const reviewTexts = document.querySelectorAll('.review-text');
+
+    
+    readMoreLinks.forEach((readMore, index) => {
+        readMore.addEventListener('click', function() {
+            reviewTexts[index].style.height = 'auto';
+            readMore.style.display = 'none';
+            readLessLinks[index].style.display = 'inline';
+        });
+    });
+
+    
+    readLessLinks.forEach((readLess, index) => {
+        readLess.addEventListener('click', function() {
+            reviewTexts[index].style.height = '80px'; 
+            readMoreLinks[index].style.display = 'inline';
+            readLess.style.display = 'none';
+        });
+    });
+});
+
+function updateReviews(data) {
+    const reviewsList = document.getElementById("reviewsList");
+    reviewsList.innerHTML = ""; 
+
+    const table = document.createElement("table");
+
+    data.forEach(row => {
+        const rowElement = document.createElement("tr");
+
+        const ratingCell = document.createElement("td");
+        ratingCell.textContent = row.Output;
+        ratingCell.style.color = "white";
+        ratingCell.style.fontSize = "16px";
+        ratingCell.style.fontFamily = "monospace";
+        rowElement.appendChild(ratingCell);
+
+        const reviewCell = document.createElement("td");
+        reviewCell.textContent = row.Review;
+        reviewCell.style.color = "white"; 
+        reviewCell.style.fontFamily = "monospace";
+        rowElement.appendChild(reviewCell);
+
+        table.appendChild(rowElement);
+    });
+
+    reviewsList.appendChild(table);
+}
+function createOutputMappingTable() {
+    const outputMapping = {
+        1: "Terrible",
+        2: "Bad",
+        3: "Neutral",
+        4: "Good",
+        5: "Amazing"
+    };
+
+    const tableContainer = document.getElementById("outputMappingTable");
+    const table = document.createElement("table");
+
+    
+    const headerRow = table.insertRow(0);
+    const headerCell1 = headerRow.insertCell(0);
+    const headerCell2 = headerRow.insertCell(1);
+    headerCell1.textContent = "Star Rating";
+    headerCell2.textContent = "Defintion of the Rating";
+
+    
+    let rowIndex = 1;
+    for (const key in outputMapping) {
+        const row = table.insertRow(rowIndex);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        cell1.textContent = key;
+        cell2.textContent = outputMapping[key];
+        rowIndex++;
+    }
+
+    tableContainer.appendChild(table);
+}
+
+
+document.addEventListener("DOMContentLoaded", createOutputMappingTable);
--- app.py
+++ app.py
@@ -1,28 +1,90 @@
-# 1. import Flask
+
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
 
-# 2. Create an app, being sure to pass __name__
+
+
+
 app = Flask(__name__)
 CORS(app)
 
-# 3. Define what to do when a user hits the index route
+tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+output_mapping = {
+    1: "Terrible",
+    2: "Bad",
+    3: "Neutral",
+    4: "Good",
+    5: "Amazing"
+ }
+
+
 @app.route("/", methods=["POST"])
 def home():
-    print("Server received request for 'Home' page...")
-    text = jsonify("Welcome to my Home page!")
-    return text
+    user_input1 = request.json.get('text1')
+    user_input2 = request.json.get('text2')
+    response_text = f"{user_input1} {user_input2}"
+    print("response recieved")
+
+    processed_text = response_text.replace(' ', '-').lower()
+    base_url = f'https://www.yelp.com/biz/{processed_text}'
+
+    reviews = []
+    reviews_found = False
+
+    while True:
+        r = requests.get(base_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        regex = re.compile('.*comment.*')
+        results = soup.find_all('p', class_=regex)
+        if results:  
+                reviews_found = True
+                reviews = [result.text for result in results]
+            
+       
+        next_button = soup.find('a', class_='next')
+        if not next_button:
+            break
+
+        next_button = soup.find('a', class_='next-link')  
+        next_url = next_button['href']  
+
+        
+        full_next_url = f'https://www.yelp.com{next_url}'
+    print("reviews found")
 
 
-# 4. Define what to do when a user hits the /about route
-@app.route('/process_text', methods=['POST'])
-def process_text():
-    user_input = request.form.get('user_text')
-    # Do something with user_input, e.g., process or display it
-    return f"You entered: {user_input}"
+    df = pd.DataFrame({'Review': reviews})
 
+    if not reviews_found:
+        df['Output'] = "Error:1 No Reviews Found."
+    else:
+        df['Output'] = df['Review'].apply(predict_sentiment)
+    
+    print("dataframe found")
+
+    print(df.to_string(index=False))
+    print(df.to_dict(orient='records'))
+
+    return jsonify(response = base_url, dataframe=df.to_dict(orient='records'))
+
+
+
+def predict_sentiment(review):
+       
+        encoded_text = tokenizer(review, padding=True, truncation=True, return_tensors="pt")
+
+        result = model(**encoded_text)
+        model_output = int(torch.argmax(result.logits)) + 1
+        return model_output
 
 if __name__ == "__main__":
     app.run(debug=True)
-
